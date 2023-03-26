8a448fa0611376b16e4224c5b50ce85692ab12c2
Andre Gomes <andremsgomes@gmail.com>
2023-03-26 17:42:03 +0100

    Changed migration

--- db/migrate/20230326161037_create_gauges.rb
+++ db/migrate/20230326161037_create_gauges.rb
@@ -2,10 +2,10 @@ class CreateGauges < ActiveRecord::Migration[7.0]
   def change
     create_table :gauges do |t|
       t.string :name
-      t.string :begin_date
-      t.string :end_date
-      t.string :unit
-      t.string :time_unit
+      t.datetime :begin_date
+      t.datetime :end_date
+      t.integer :unit
+      t.integer :time_unit
 
       t.timestamps
     end
