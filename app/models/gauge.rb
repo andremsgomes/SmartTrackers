1575977079a5835864cec8711d3086fae8700571
Andre Gomes <andremsgomes@gmail.com>
2023-03-28 11:27:28 +0100

    Show gauge total measured on the footer of the entries list

--- app/models/gauge.rb
+++ app/models/gauge.rb
@@ -3,4 +3,8 @@ class Gauge < ApplicationRecord
   enum time_unit: { days: 0, weeks: 1, months: 2, years: 3 }
 
   has_many :gauge_entries, dependent: :destroy
+
+  def total_measured
+    gauge_entries.sum(&:value)
+  end
 end
